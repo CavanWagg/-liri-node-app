c1af12ecd0f62921b3398ccd76eed7e62a216de4
Cavan Wagg <waggx002@gmail.com>
2018-04-18 21:55:38 -0500

    spotify search complete, readText function in progress, needs to call other functions

--- liri.js
+++ liri.js
@@ -9,7 +9,7 @@ const keys = require('./key.js');
 
 
 // User based authentication
-// var spotify = new Spotify(keys.spotify);
+const spotify = new Spotify(keys.spotify);
 const client = new Twitter(keys.twitter);
 
 // switch statement for all the possibilities
@@ -45,7 +45,7 @@ function movieThis() {
   let movieName = '';
 
   // if no argument is specified then use mr. nobody
-  if (process.argv[3] == undefined) {
+  if (process.argv[3] === undefined) {
     movieName = 'mr+nobody';
   } else {
   // Loop through all the words in the node argument
@@ -93,10 +93,54 @@ function myTweets() {
 }
 
 function spotifySong() {
+  const nodeArgs = process.argv;
+
+  // Create an empty variable for holding the movie name
+  let songSearch = '';
+
+  // if no argument is specified then use mr. nobody
+  if (process.argv[3] === undefined) {
+    songSearch = 'the sign';
+  } else {
+  // Loop through all the words in the node argument
+    // And do a little for-loop magic to handle the inclusion of "+"s
+    for (let i = 3; i < nodeArgs.length; i++) {
+      if (i > 3 && i < nodeArgs.length) {
+        songSearch = `${songSearch} ${nodeArgs[i]}`;
+      } else {
+        songSearch += nodeArgs[i];
+      }
+    }
+  }
+  spotify.search({ type: 'track', query: songSearch, limit: 1 }, (err, data) => {
+    if (err) {
+      return console.log(`Error occurred: ${err}`);
+    }
+
+    console.log(`Artists: ${JSON.stringify(data.tracks.items[0].artists[0].name)}`);
+    console.log(`Song: ${JSON.stringify(data.tracks.items[0].name)}`);
+    console.log(`Preview link: ${JSON.stringify(data.tracks.items[0].preview_url)}`);
+    console.log(`Album: ${JSON.stringify(data.tracks.items[0].album.name)}`);
+  });
+}
+
+function readText() {
+  fs.readFile('random.txt', 'utf8', (error, data) => {
+    // If the code experiences any errors it will log the error to the console.
+    if (error) {
+      return console.log(error);
+    }
+    // Then split it by commas (to make it more readable)
+    const dataArr = data.split(',');
+    // let dataArr[0]
+    // let dataArr[1]
 
 
+    // We will then re-display the content as an array for later use.
+    console.log(dataArr);
+  });
 }
 
 function noInput() {
-  console.log('Check a movie, spotify a song, or view tweets');
+  console.log('I did not recognize that input, please use "movie-this", "spotify-this-song", "my-tweets", or "do-what-it-says"');
 }
